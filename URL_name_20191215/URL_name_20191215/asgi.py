437351d9286d487ad920e9b5684e3602a1158bc2
1924zjy0835 <1924083517@qq.com>
2019-12-15 09:00:11 +0800

    20191215

---
+++ URL_name_20191215/URL_name_20191215/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for URL_name_20191215 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'URL_name_20191215.settings')
+
+application = get_asgi_application()
